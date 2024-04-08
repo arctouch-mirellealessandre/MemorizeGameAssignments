ec30a2a1d3157492f45ba3766ab20729ec5c7434
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-04-08 09:37:58 -0300

    Initial Commit

---
+++ MemorizeGameAssignments/MemorizeGameAssignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameAssignmentsApp.swift
+//  MemorizeGameAssignments
+//
+//  Created by Mirelle Alessandre on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameAssignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
